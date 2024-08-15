ebee7a4e7d8404a122cc1ad0e8897287f1ded61d
bruno-yuji <bruno.ytakahashi@sptech.school>
2024-08-14 21:03:41 -0300

    adicionando primeira versão do banco de dados

---
+++ TagTech_DB.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE IF NOT EXISTS TagTech;
+USE TagTech ;
+
+CREATE TABLE empresa (
+  idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+  razaoSocial VARCHAR(80),
+  nomeFantasia VARCHAR(80),
+  cnpj CHAR(14),
+  cep CHAR(8)
+);
+
+CREATE TABLE usuario (
+  idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(45),
+  email VARCHAR(45),
+  senha VARCHAR(45),
+  cargo VARCHAR(45),
+  telefone VARCHAR(45),
+  fkEmpresa INT NOT NULL,
+  FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
+);
+
+CREATE TABLE maquina (
+  idMaquina INT PRIMARY KEY AUTO_INCREMENT,
+  nomeMaquina VARCHAR(45),
+  sistemaOperacional VARCHAR(45),
+  fkEmpresa INT NOT NULL,
+  FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
+);
+
+CREATE TABLE historico (
+  idHistorico INT PRIMARY KEY AUTO_INCREMENT,
+  momento DATETIME, 
+  velocidadeCpu DOUBLE,
+  consumoMemoria DOUBLE,
+  atividadeDisco DOUBLE,
+  taxaTransferenciaRede DOUBLE,
+  fkMaquina INT NOT NULL,
+  FOREIGN KEY (fkMaquina) REFERENCES maquina (idMaquina)
+);
+
+CREATE TABLE usuarioResponsavelMaquina (
+  fkUsuario INT NOT NULL,
+  fkMaquina INT NOT NULL,
+  PRIMARY KEY (fkUsuario, fkMaquina),
+  FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario),
+  FOREIGN KEY (fkMaquina) REFERENCES maquina (idMaquina)
+);
